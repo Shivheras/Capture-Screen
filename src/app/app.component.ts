5a0d78f5e8d9738ae25101d7e737c7b7340f9a51
Shivheras <shivheras@gmail.com>
2020-10-29 22:10:38 +0530

    commit code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Screenshot';
+  ngOnInit()
+{
+  
+}
+capture()
+{
+
+  let elem = document.getElementById("scream");
+  html2canvas(elem).then(function (canvas) {
+    let generatedImage = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    let a = document.createElement('a');
+    a.href = generatedImage;
+    a.download = `image.png`;
+    a.click();
+  });
+}
 }
